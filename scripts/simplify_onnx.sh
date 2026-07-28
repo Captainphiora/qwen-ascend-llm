13539c4ded6ae17ea27de62550170ef6c0a0448a
Captainphiora <Captainphiora@163.com>
2026-07-28 16:06:00 +0800

    init

--- scripts/simplify_onnx.sh
+++
@@ -1,6 +0,0 @@
-# INPUT="output/onnx2_DeepSeek-R1-Distill-Qwen-1.5B_4096/DeepSeek-R1-Distill-Qwen-1.5B_4096_rectified.onnx"
-INPUT="output/onnx_test_DeepSeek-R1-Distill-Qwen-1.5B_4096/DeepSeek-R1-Distill-Qwen-1.5B_4096.onnx"
-OUTPUT="output/onnx2_DeepSeek-R1-Distill-Qwen-1.5B_4096_simplified/DeepSeek-R1-Distill-Qwen-1.5B_4096_rectified_sim.onnx"
-python export/simplify_onnx.py \
-    --input $INPUT \
-    --output $OUTPUT
